0b5b9809fe138466ff9f7763bb3740a11424e01d
KavyaModiboena <163985670+KavyaModiboena@users.noreply.github.com>
2024-04-13 14:04:02 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name:'Loki',
+        des:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur iste reprehenderit, ullam dicta qui, aspernatur, impedit officiis dolores temporibus pariatur cupiditate vel quasi ipsam unde facere optio sint sunt ea.',
+        image:'slider 1.PNG'
+    },
+    {
+        name:'falcon and the winter solider',
+        des:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur iste reprehenderit, ullam dicta qui, aspernatur, impedit officiis dolores temporibus pariatur cupiditate vel quasi ipsam unde facere optio sint sunt ea.',
+        image:'slider 2.PNG'
+    },
+    {
+        name:'Wanda and vision',
+        des:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur iste reprehenderit, ullam dicta qui, aspernatur, impedit officiis dolores temporibus pariatur cupiditate vel quasi ipsam unde facere optio sint sunt ea.',
+        image: 'slider 3.PNG'
+    },
+    {
+        name: 'raya and the last dragon',
+        des:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur iste reprehenderit, ullam dicta qui, aspernatur, impedit officiis dolores temporibus pariatur cupiditate vel quasi ipsam unde facere optio sint sunt ea.',
+        image: 'slider 4.PNG'
+    },
+    {
+        name: 'luca',
+        des:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur iste reprehenderit, ullam dicta qui, aspernatur, impedit officiis dolores temporibus pariatur cupiditate vel quasi ipsam unde facere optio sint sunt ea.',
+        image: 'slider 5.PNG'
+    }
+]
